2253066cd27bf20abdcb86610adf4a5b1ebecd7d
Tatiana Rem <tatiana13d@yandex.ru>
2023-07-28 19:15:53 +0600

    Initial commit

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,40 @@
 const sorting = require("../../app");
+const sortByName = sorting.sortByName;
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ]
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ]
+
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Book names are duplicated in the list", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    const expected = [
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(expected);
   });
 });
