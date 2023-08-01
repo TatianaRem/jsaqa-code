d379ab39d3976d314b61f37f4de11b13d260b6c9
Tatiana Rem <tatiana13d@yandex.ru>
2023-08-01 23:29:24 +0600

    New 3 tests were added

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -13,22 +13,64 @@ describe("Github page tests", () => {
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
+  });
+});
+test("The first link attribute", async () => {
+  const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+  expect(actual).toEqual("#start-of-content");
+}, 15000);
+
+test("The page contains Sign in button", async () => {
+  const btnSelector = ".btn-large-mktg.btn-mktg";
+  await page.waitForSelector(btnSelector, {
+    visible: true,
+  });
+  const actual = await page.$eval(btnSelector, (link) => link.textContent);
+  expect(actual).toContain("Get started with Team");
+}, 15000);
+
+const { setup, teardown } = require("./jest-puppeteer.config.js");
+
+describe("Creating account Github page tests", () => {
+  beforeEach(async () => {
+    await setup();
+    await page.goto(
+      "https://github.com/join?plan=business&ref_cta=Get%2520started%2520with%2520Team&ref_loc=team-page-footer&ref_page=%2Fteam"
+    );
   });
 
-  test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+  afterEach(async () => {
+    await teardown();
   });
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
+  test("Header with selector h1", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title3 = await page.title();
+    expect(title3).toEqual("Join GitHub · GitHub");
+  }, 15000);
+
+  test("The header of verification block", async () => {
+    const f4Selector = ".mb-3.f4";
+    await page.waitForSelector(f4Selector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    const actual = await page.$eval(f4Selector, (link) => link.textContent);
+    expect(actual).toContain("Verify your account");
+  }, 15000);
+
+  test("The using link attribute", async () => {
+    const actual = await page.$eval("span", (link) =>
+      link.getAttribute("class")
+    );
+    expect(actual).toEqual(
+      "progress-pjax-loader Progress position-fixed width-full"
+    );
+  }, 15000);
 });
--- 7.4/puppeteer/jest-puppeteer.config.js
+++ 7.4/puppeteer/jest-puppeteer.config.js
@@ -1,9 +1,25 @@
-module.exports = {
-    launch: {
-        headless: false,
-        defaultViewport: null,
-        args: ['--start-maximized'] //— используем максимальный размер окна браузера
-      },
-    
-  };
-  
+const puppeteer = require("puppeteer");
+
+const browserOptions = {
+  launch: {
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"], //— используем максимальный размер окна браузера
+  },
+};
+
+let browser;
+let page;
+
+async function setup() {
+  browser = await puppeteer.launch(browserOptions.launch); //Для запуска браузера с настройками указанными выше
+  page = await browser.newPage();
+}
+
+async function teardown() {
+  //Функция для завершения после каждого теста
+  await page.close();
+  await browser.close();
+}
+
+module.exports = { setup, teardown };
